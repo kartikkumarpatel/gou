7b252ab6bac88388cad5910bdd196cc2946cc6a2
Kartik Patel <kartik@Kartiks-MacBook-Pro.local>
2018-04-01 16:53:11 +1200

    about growz on you

---
+++ about.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include('includes/header.php'); ?>
+	<title>About GOU</title>
+</head>
+<body>
+	<div class="content-container">
+		<?php include('includes/nav.php'); ?>
+
+		<div class="center-text">
+			<p>
+				The opening of Growz On U Garden Centre is the culmination of 104 years of plant knowledge, nursery, garden landscaping and garden centre experience of Paul and Irene Green and their three children.
+			</p>
+			<p>
+				Michael, Landscape Manager of All Garden Works. He oversees the implementation of all facets of landscaping including: site preparation, decks, screens, pergolas, water features, fences, retaining walls, irrigation, lawns, concrete work, cobblestone paving, swimming pool installation, rock gardens, tree and shrub planting. Lisa Stott, who propagates a range of plants and grows them on in our own nursery, as well as very capably assisting in the retail outlet - Growz On U at Totara. Paulette Smith who runs a very smooth office keeping track of indented orders, printing labels, paying wages, PAYE, GST and handling all the Debtors and Creditors, as well as telephone receptionist. 
+			</p>
+			<p>
+				Paul commenced a Horticulture and Gardening apprenticeship of 10,000 hours (5 years) with the Tourist and Publicity Department in Rotorua, working at the Government Gardens. This work entailed raising and planting flower plants for these showcase gardens, the care of the 4000 roses in the rose beds, spraying, pruning and deheading and much weeding. At the time, the Government Gardens boasted a very large display glasshouse similar, although a lot smaller than the greenhouses in the famous Kew Gardens. This greenhouse was heated with steam from the underground bores (as was all the greenhouses in the nursery) and a wide range of very exotic plants thrived under the constant care of a dedicated team of gardeners. 
+			</p>
+			<p>
+				After 3 years, Paul transferred his apprenticeship to the world renown firm of Duncan and Davies in New Plymouth. Commercial nursery work was far removed from the Government funded Government Gardens and assisting with production of a staggering range of trees and shrubs, further entrenched a love of Horticulture and all things associated with the garden. Paul and Irene met at Duncan & Davies, Irene was employed in the propagation department where up to 12 'prop girls' produced cuttings of the many tens of thousands of trees and shrubs Duncan & Davies produced annually. 
+			</p>
+			<p>
+				Irene and Paul spent 6 years at Duncan & Davies then decided to move North to Thames and open up a derelict Frasers Garden Centre that had been closed down. This was renamed Totara Garden Centre and was run successfully for 6-7 years before Paul & Irene purchased a block of land closer to town and opened a new Garden Centre (Aero View) which they ran for 10 years. This thriving business was sold and a lifestyle block was purchased in the Kauaeranga Valley. Paul worked at various jobs including Landscaping, Building and finally back to landscaping in 2001, when he was joined by son Michael. The business grew quickly and in 2006 Paul & Irene made a decision to open another Garden Centre (Growz On U) and the site - the same site where Paul and Irene started in Thames in 1972. 
+			</p>
+			<p>
+				We made a commitment to stock a wide range of interesting plants, keep the quality high and the prices great value for money, and judging from the huge interest and great ongoing support we know we have struck a chord with the gardening public of Thames, The Coromandel Peninsula, Hauraki Plains, right down to Waihi Beach and the Seabird Coast. 
+			</p>
+			<p>
+				Our object is simply this - 'Happy Customers are Happy Gardeners'. And we try to go that little bit further to keep our customers coming back time after time. <br><br>
+				Happy Gardening Paul, Irene & Families
+			</p>
+		</div>
+	</div>
+</body>
+</html>
