422f4e2dec8583da4a5185d754b3ece24caf705f
Kartik Patel <kartik@Kartiks-MacBook-Pro.local>
2018-04-07 18:31:29 +1200

    contact complete

--- contact.php
+++ contact.php
@@ -1,3 +1,22 @@
+<?php 
+	if (isset($_POST['submit'])) {
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$message = $_POST['message'];
+
+
+		$mailto = "kartik.95@live.com"; 
+		$subject = "From: ".$name;
+		$message = "Hi, my name is $name. \n\n
+					My name address is $email. \n\n
+					My enquiry is:\n$message";
+
+		mail($mailto, $subject, $message);
+		header("Location: contact.php?mailsend");
+	}
+	
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -26,22 +45,24 @@
 			<p>Or you can fill out this enquirey form bellow and we will get back to you as soon as possable. Thank You</p>
 
 			<div class="message-form">
-				<form>
+				<form method="post" action="contact.php">
 				  <div class="form-group">
 				    <label for="name">Name</label>
-				    <input type="text" class="form-control" id="name" placeholder="Enter Name" style="width: 300px;">
+				    <input type="text" name='name' class="form-control" id="name" placeholder="Enter Name" style="width: 300px;">
 				  </div>
 				  <div class="form-group">
 				    <label for="email">Email</label>
-				    <input type="text" class="form-control" id="email" placeholder="Enter Email" style="width: 300px;">
+				    <input type="text" name='email' class="form-control" id="email" placeholder="Enter Email" style="width: 300px;">
 				  </div>
-				</form>
 				<div class="form-group">
 				    <label for="message">Message</label>
-				    <textarea type="text" class="form-control" id="message" placeholder="Enter Message" rows="5"></textarea> 
+				    <textarea type="text" name="message" class="form-control" id="message" placeholder="Enter Message" rows="5"></textarea> 
 				 </div>
-				 <button type="button" class="btn btn-default">Reset</button>
-				 <button type="button" class="btn btn-deafult">Submit Enquiry</button>
+
+
+				 <input type="reset" name="reset" class="btn btn-default" value="Reset" />
+				 <input type="submit" name="submit" class="btn btn-deafult" value="Submit" />
+				</form>
 			</div>
 		</div>
 
@@ -57,9 +78,8 @@
 				</div>
 			</div>
 		</div>
-
-
 	</div>
+
 	<div>
 		<?php include('includes/footer.php'); ?>
 	</div>
