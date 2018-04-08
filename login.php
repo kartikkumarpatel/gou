be567d853cdd568e0bbba98cf0f0074e1c103a60
Kartik Patel <kartik@Kartiks-MacBook-Pro.local>
2018-04-08 20:35:03 +1200

    logging in user

--- login.php
+++ login.php
@@ -6,15 +6,15 @@
 		$password = md5($_POST['password']);
 
 		$user = "SELECT * FROM members WHERE email = '$email' AND password = '$password'";
-		$execute_user = mysqli_query($dbconn, $user) or die (mysqli_error());
+		$execute_user = mysqli_query($dbconn, $user);
 		$data = mysqli_fetch_assoc($execute_user);
 
 		if (empty($email) || empty($password )) {
 			$message = "One of the fields have been left blank.";
 		}else if(mysqli_num_rows($execute_user) > 0) {
-			// start_session();
-			// $_SESSION['email'] = '$email';
-			// $_SESSION['email'] = $data['email'];
+			start_session();
+			$_SESSION['email'] = '$email';
+			$_SESSION['email'] = $data['email'];
 			$message = "loged in";
 		}else{
 			$message = "Incorrect email or password";
