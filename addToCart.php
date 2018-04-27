662ed941c5590ac514abf70a939568939f126e6f
Kartik Patel <kartik@Kartiks-MacBook-Pro.local>
2018-04-27 17:43:04 +1200

    cart styled

--- addToCart.php
+++ addToCart.php
@@ -2,7 +2,7 @@
 	session_start();
 
 	if (isset($_GET['nm'])) {
-		$_SESSION['cart'][] = array("img" => $_GET['img'], "nm" => $_GET['nm'], "type" => $_GET['type'], "price" => $_GET['price'], "qty" => 1);
+		$_SESSION['cart'][] = array("img" => $_GET['img'], "nm" => $_GET['nm'], "price" => $_GET['price'], "qty" => 1);
 	}
 
 	header("location: shop.php");
--- cart.php
+++ cart.php
@@ -1,17 +1,6 @@
 <?php 
 	session_start();
 	require_once 'includes/dbconn.php';
-
-	if (isset($_GET['typeID'])) {
-		$type_id = $_GET['typeID'];
-	}else{
-		$type_id = 1;
-	}
-
-	$type_sql = "SELECT typeID, type FROM type";
-	$type_query = mysqli_query($dbconn, $type_sql) or die(mysqli_error()); //executes the query
-	$type_nav = mysqli_fetch_assoc($type_query); //fetches the array
-	$type_count = mysqli_num_rows($type_query);
 ?>
 
 <!DOCTYPE html>
@@ -26,31 +15,43 @@
 		
 		<div class="container-fluid">
 				<div>
-					<table class="table table-hover" border="1px" width="50%">
+					<table class="table-hover" border="1px" width="80%" style="line-height: 30px; margin-left: auto; margin-right: auto;">
 						<tr>
 						    <th>Image</th>
 						    <th>Name</th> 
-						    <th>Type</th>
 						    <th>Price</th>
 						    <th>Quanity</th>
 						    <th>Total Price</th>
+						    <th>Delete</th>
 						  </tr>
 
 						  <?php
 						  	foreach ($_SESSION['cart'] as $id => $value) {
-						  		$total_price = $value['qty'] * $value['price'];
+						  		$rose_price = $value['qty'] * $value['price'];
+						  		$total_price += $value['price'];
 
 						  		echo '<tr>
 									    <td>'.$value['img'].'</td>
 									    <td>'.$value['nm'].'</td> 
-									    <td>'.$value['type'].'</td>
 									    <td>'.$value['price'].'</td>
 									    <td><input type="text" name="quanity" value="'.$value['qty'].'" size="2"></td>
-									    <th>$'.$total_price.'</th>
+									    <td>$'.$rose_price.'</td>
+									    <td style="width:10px;"><button class="btn btn-default">Remove</button></td>
 									  </tr>';
 						  	}
+
+					  			echo '<tr>
+								  		<td colspan="4" style="text-align:right;"><strong>Total:</strong></td>
+								  		<td><strong>$'.$total_price.'</strong></td>
+								 	  </tr>';
 						  ?>
 					</table>
+					
+					<div class="cart-buttons">
+						<span><button class="btn btn-default" style="padding-right: 10px;">Update</button></span>
+						<span><button class="btn btn-default">Proceed</button></span>
+					</div>
+
 				</div>
 			</div>		
 		</div>
