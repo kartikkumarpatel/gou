2c1f2e99ec4a6552a5e3a1e87321b3fd6c9368aa
Kartik Patel <kartik@Kartiks-MacBook-Pro.local>
2018-04-28 20:20:00 +1200

    redirection on logout to homepage

--- logout.php
+++ logout.php
@@ -1,24 +1,6 @@
 <?php
 	session_start();
 	session_destroy();
+	header("Location: index.php");
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<?php include 'includes/header.php'; ?>
-	<title>Logged Out</title>
-</head>
-<body>
-	<div class="content-container">
-		<?php include 'includes/nav.php' ?>
-		
-		<div class=" login-form well">
-			<h4 class="logged-out">You have sucessfully logged out! <br><br> Thank You</h4>
-		</div>
-	</div>
-	<div>
-		<?php include('includes/footer.php'); ?>
-	</div>
-</body>
-</html>
